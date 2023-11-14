ad9eb96e9e2752e056317495f8ecefa57509936b
Mideypapi <abioye.faruok@gmail.com>
2023-11-14 14:29:55 -0800

    printf

--- printf.c
+++ printf.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -9,53 +11,15 @@
 
 int _printf(const char *format, ...)
 {
-	int character_printer = 0;
-	va_list list;
+	va_list args;
+	int length = 0;
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(list, format);
+	va_start(args, format);
 
-	while (*format)
-	{
-		if (*format != "%")
-		{
-			write(1, format, 1);
-			character_printer++;
-		}
-		else
-		{
-			format++;
-			if (*format == "/0")
-				break;
-			if (*format == "c")
-			{
-				char c = va_arg(list, int);
-
-				write(1, % c, 1);
-				character_printer++;
-			}
-			else if (*format == "%")
-			{
-				write(1, format, 1);
-				character_printer++;
-			}
-			else if (*format ==  "s")
-			{
-				char *str = va_arg(list, char*);
-				int str_len = 0;
-
-				while (str[str_len] != "\0")
-					str_len++;
-
-				write(1, str, str_len);
-				character_printer += str_len;
-			}
-		}
-		format++;
-	}
-	va_end(list);
-
-	return (character_printer);
+	length = _print_format(format, args);
+	va_end(args);
+	return (length);
 }
